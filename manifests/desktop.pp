12869de1bbc47423d5d8e589d3f14ad6ce06c9cc
Brian Henderson <theinitialsarebh@gmail.com>
2019-12-02 20:00:04 -0800

    use ldap module

--- manifests/desktop.pp
+++ manifests/desktop.pp
@@ -1,6 +1,5 @@
 node /desktop\d+\./ {
     package { 'squid-deb-proxy-client':     ensure => 'installed' }
 
-
-    package { 'libpam-ldapd':               ensure => 'installed' }
+    include 'ldap::client'
 }
