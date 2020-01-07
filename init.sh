3813e7986e629810cfb547bc9c9fa674f104fdc5
Brian Henderson <theinitialsarebh@gmail.com>
2020-01-06 18:31:58 -0800

    bootstrap dns

--- init.sh
+++ init.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+sudo hostname rpi-desktop00.henderb.net
+
 sudo apt install -y squid-deb-proxy-client
+
+SERVER_IP=$(avahi-resolve -4 rpi-server.local | cut -f2)
+echo "${SERVER_IP}   rpi-server.henderb.net" >> /etc/hosts
+
 sudo apt update
 sudo apt install -y puppet
 sudo puppet apply --modulepath=modules/ .
