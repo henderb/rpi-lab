386b6b62eca75908ba9a820b169533d5e62dcb9c
Brian Henderson <theinitialsarebh@gmail.com>
2020-01-06 16:43:02 -0800

    add files I'm sure I changed

--- modules/apt_cache_ng/manifests/server.pp
+++ modules/apt_cache_ng/manifests/server.pp
@@ -1,4 +1,10 @@
 class apt_cache_ng::server {
     package { 'apt-cacher-ng':  ensure => 'installed' }
     package { 'avahi-daemon':   ensure => 'installed' }
+
+    service { 'apt-cacher-ng':
+        ensure  => 'running',
+        enable  => true,
+        require => Package['apt-cacher-ng'],
+    }
 }
--- modules/ldap/manifests/client.pp
+++ modules/ldap/manifests/client.pp
@@ -1,6 +1,11 @@
-class ldap::client {
+class ldap::client(
+        String $ldap_base  = 'dc=henderb,dc=net',
+        String $ldap_uri   = 'ldap://rpi-server.henderb.net',
+    ){
     
     package { 'libpam-ldapd':   ensure => 'installed' }
+    package { 'libnss-ldapd':   ensure => 'installed' }
+    package { 'autofs-ldap':    ensure => 'installed' }
 
     file { '/etc/nsswitch.conf':
         ensure  => 'present',
@@ -9,5 +14,31 @@ class ldap::client {
         owner   => 'root',
         group   => 'root',
         require => Package['libpam-ldapd'],
+    }
+
+    file { '/etc/default/autofs':
+        ensure  => 'present',
+        content => template('ldap/autofs.erb'),
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
+        require => Package['autofs-ldap'],
+    }
+
+    file { '/etc/nslcd.conf':
+        ensure  => 'present',
+        content => template('ldap/nslcd.conf.erb'),
+        mode    => '0640',
+        owner   => 'root',
+        group   => 'nslcd',
+        require => Package['libnss-ldapd'],
+    }
+
+    file { '/etc/lightdm/lightdm.conf':
+        ensure  => 'present',
+        source  => 'puppet:///modules/ldap/lightdm.conf',
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
     }
 }
