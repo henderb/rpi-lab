a90a92811359cc7e05805484f834e4e6eafea3f5
Brian Henderson <henderb@brian-laptop-ubuntu.henderb.net>
2020-02-02 15:55:52 -0800

    cron puppet applyw

---
+++ modules/puppet-cron/manifests/init.pp
@@ -0,0 +1,22 @@
+class puppet-cron {
+    cron { 'puppet-apply':
+        ensure  => 'present',
+        user    => 'root',
+        minute  => 60,
+        command => '/usr/bin/puppet apply /etc/puppet/code --logdest syslog',
+    }
+
+    vcsrepo { '/etc/puppet/code':
+        ensure   => 'present',
+        provider => 'git',
+        source   => 'https://github.com/henderb/rpi-lab.git',
+        revision => 'master',
+    }
+
+    exec { 'puppet-apply':
+	command     => '/usr/bin/puppet apply /etc/puppet/code --logdest syslog'
+        refreshonly => true,
+        user        => 'root',
+        subscribe   => Vcsrepo['/etc/puppet/code'],
+    }
+}
