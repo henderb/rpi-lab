1bfc5857bec53ca19c2c9a09bc204dd8ace2fa44
Brian Henderson <theinitialsarebh@gmail.com>
2020-02-11 05:12:28 -0800

    set homepage

--- manifests/desktop.pp
+++ manifests/desktop.pp
@@ -2,4 +2,5 @@ node /^rpi-desktop\d+\.ctmug\.net$/ {
     include 'apt_cache_ng::client'
     include 'ldap::client'
     include 'puppet_cron'
+    include 'desktop'
 }
--- modules/desktop/manifests/init.pp
+++ modules/desktop/manifests/init.pp
@@ -6,4 +6,12 @@ class desktop {
         group  => 'root',
         mode   => '0644',
     }
+
+    file { '/etc/chromium-browser/policies/recommended/master_preferences':
+        ensure => 'present',
+        source => 'puppet:///modules/desktop/master_preferences',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+    }
 }
